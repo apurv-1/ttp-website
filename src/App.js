a4f840f4e1638d1c9c9f50a16c2a76c035c36d10
Apurv Gupta <apurv3351@gmail.com>
2021-05-13 19:25:00 +0530

    Initialised

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import Programme from "./Components/Programme";
+import Topics from "./Components/Topics";
+import Speakers from "./Components/Speakers";
+import Registrations from "./Components/Registration";
+import College from "./Components/College";
+import University from "./Components/University";
+import Coordinators from "./Components/Coordinators";
+import Footer from "./Components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">Hello
+      {/* <Navbar />
+      <Home />
+      <Programme />
+      <Topics />
+      <Speakers />
+      <Registrations />
+      <College />
+      <University />
+      <Coordinators />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/Components/College.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const College = () => {
+  return (
+    <div>
+      <h1>SSIPMT Raipur</h1>
+    </div>
+  );
+};
+
+export default College;

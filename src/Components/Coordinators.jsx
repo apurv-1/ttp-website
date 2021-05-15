71a03fa88a625a05505eaf59779b3382f09a7933
Abhinav Dubey <dubey.abhinav76@gmail.com>
2021-05-15 19:38:23 +0530

    layout of contents created

--- src/Components/Coordinators.jsx
+++ src/Components/Coordinators.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import classes from "../Styles/coordinators.module.css";
+import React from 'react';
+import classes from '../Styles/coordinators.module.css';
 
 const Coordinators = () => {
   return (
-    <div className={classes.root}>
-      {/* <h1>Coordinators</h1> */}
+    <div className="container pt-5">
+      <div className="text-center pb-3">
+        <h3 className="text-primary pb-3">Host Institute Patrons</h3>
+        <h4>Shri I.P. Mishra (Chairman, SGES)</h4>
+        <h4>Shri Nishant Tripathi (Chairman (BG), SSIPMT, Raipur)</h4>
+        <h4>Dr. Alok Kumar Jain, Principal, SSIPMT, Raipur</h4>
+      </div>
+      <div className="row">
+        <div className="col-md-6 col-sm-12">
+          <div className="text-center pb-3">
+            <h3 className="text-primary pb-3">Program Co-ordinator</h3>
+            <h4>Dr. J P Patra, Department of CSE, SSIPMT, Raipur</h4>
+            <h4>Mobile No.8319527740 (9522219109, Whatsapp) </h4>
+            <h4>Email: jp.patra@ssipmt.com</h4>
+          </div>
+        </div>
+        <div className="col-md-6 col-sm-12">
+          <div className="text-center pb-3">
+            <h3 className="text-primary pb-3">Program Co-ordinator</h3>
+            <h4>Mr. Anand Tamrakar, Department of CSE, SSIPMT, Raipur</h4>
+            <h4>Mobile No: 9993090232 (Whatsapp)</h4>
+            <h4>Email: a.tamrakar@ssipmt.com</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,8 +1,60 @@
-import React from "react";
-import classes from "../Styles/registration.module.css";
+import React from 'react';
+import classes from '../Styles/registration.module.css';
 
 const Registration = () => {
-  return <div className={classes.root}></div>;
+  const onSumbitHandle = (e) => {
+    e.preventDefault();
+    const link =
+      'https://docs.google.com/forms/d/e/1FAIpQLSfFFWM_y0JCi78Mv6Lzf69UYqfNHJDY22eC6TBA_h3bMHyd5A/viewform';
+    window.open(link, '_blank');
+  };
+
+  return (
+    <div className="container">
+      <div className="pb-4">
+        <h1 className="text-center pb-4 text-primary" style={{ color: 'red' }}>
+          Certification
+        </h1>
+        <h4 style={{ color: 'red' }}>
+          Certificates will be issued subject to the participants qualifying the
+          following conditions:
+        </h4>
+        <p>
+          a) Minimum 80% attendance.
+          <br />
+          b) Minimum 60% marks in the exam conducted on last day of the program.
+        </p>
+      </div>
+      <div>
+        <h1 className="text-center pb-4 text-primary">Registration</h1>
+        <div className="pb-3">
+          <h3 className="text-danger pb-2">Who can participate ?</h3>
+          <p>
+            Faculty from AICTE approved institutions across the country of
+            relevant disciplines. Priority will be given to faculty from CSVTU
+            affiliated institutions. Batch Size is limited to maximum 100
+            participants.
+          </p>
+        </div>
+        <div className="pb-3">
+          <h3 className="text-danger pb-2">Eligible Disciplines:</h3>
+          <p>All branches of Engineering, MBA, MCA and Polytechnic.</p>
+        </div>
+        <p className="text-center" style={{ color: 'red' }}>
+          (No Registration Fees & Participation on First Come First Serve)
+        </p>
+        <div className="col-md-12 text-center pb-3">
+          <button className="btn btn-dark" onClick={(e) => onSumbitHandle(e)}>
+            Register here
+          </button>
+        </div>
+        <div className="text-center">
+          <h4 style={{ color: 'red' }}>Last date For Registration:</h4>
+          <h5>22nd July 2021, Thursday</h5>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
